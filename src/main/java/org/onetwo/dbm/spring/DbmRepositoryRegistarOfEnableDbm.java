c87f59307ecc20caeb7de10f52533c349ec899ba
wayshall <weishao.zeng@gmail.com>
2017-07-24 17:49:06 +0800

    bugfix

--- src/main/java/org/onetwo/dbm/spring/DbmRepositoryRegistarOfEnableDbm.java
+++ src/main/java/org/onetwo/dbm/spring/DbmRepositoryRegistarOfEnableDbm.java
@@ -2,11 +2,13 @@ package org.onetwo.dbm.spring;
 
 import org.onetwo.common.db.dquery.RichModelAndQueryObjectScanTrigger;
 import org.onetwo.common.spring.SpringUtils;
+import org.onetwo.common.utils.LangUtils;
 import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
 import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.AnnotationMetadata;
+import org.springframework.util.ClassUtils;
 
 public class DbmRepositoryRegistarOfEnableDbm implements ImportBeanDefinitionRegistrar {
 	
@@ -23,7 +25,11 @@ public class DbmRepositoryRegistarOfEnableDbm implements ImportBeanDefinitionReg
 		}
 		
 		RichModelAndQueryObjectScanTrigger register = new RichModelAndQueryObjectScanTrigger(registry);
-		register.setPackagesToScan(attributes.getStringArray("packagesToScan"));
+		String[] packagesToScan = attributes.getStringArray("packagesToScan");
+		if(LangUtils.isEmpty(packagesToScan)){
+			packagesToScan = new String[]{ClassUtils.getPackageName(importingClassMetadata.getClassName())};
+		}
+		register.setPackagesToScan(packagesToScan);
 		register.setEnableRichModel(attributes.getBoolean("enableRichModel"));
 		register.scanAndRegisterBeans((ListableBeanFactory)registry);
 	}
