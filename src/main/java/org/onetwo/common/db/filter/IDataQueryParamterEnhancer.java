a813e11ab224116c3afe2b7b7701735c17b1c6c6
wayshall <weishao.zeng@gmail.com>
2018-05-31 11:07:05 +0800

    add comment

--- src/main/java/org/onetwo/common/db/filter/IDataQueryParamterEnhancer.java
+++ src/main/java/org/onetwo/common/db/filter/IDataQueryParamterEnhancer.java
@@ -6,6 +6,12 @@ import org.onetwo.common.db.sqlext.ExtQuery;
 
 public interface IDataQueryParamterEnhancer {
 	
+	/****
+	 * 返回的map会追加到条件查询，如果原来的query查询条件里已存在，则忽略
+	 * @author wayshall
+	 * @param query
+	 * @return
+	 */
 	public Map<Object, Object> enhanceParameters(ExtQuery query);
 
 }
