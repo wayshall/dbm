ad7e9481879b8b76a0db83b01b6d74b2c542fc7c
wayshall <weishao.zeng@gmail.com>
2018-10-12 14:04:04 +0800

    fix:
    vue template fix

--- src/test/java/org/onetwo/common/db/generator/DatasourceGeneratorTest.groovy
+++ src/test/java/org/onetwo/common/db/generator/DatasourceGeneratorTest.groovy
@@ -17,10 +17,10 @@ class DatasourceGeneratorTest {
 					.projectPath($/D:\mydev\js\workspace\neo-vue-admin/$)
 					.pageFileBaseDir($/D:\mydev\js\workspace\neo-vue-admin\src\views/$)
 					.stripTablePrefix("admin_")
-					.webadminGenerator("admin_user")//要生成的表名
+					.webadminGenerator("admin_application")//要生成的表名
 //						.generateVueCrud()//会在api目录生成userApi.js，在/views/adminModule/目录生成userMgr.vue和userMgrForm.vue两个文件，包含了基本的增删改查
 //						.generateVueMgrForm()
-//						.generateVueMgr()
+						.generateVueMgr()
 					.end()
 					.build()
 					.generate();//生成文件
