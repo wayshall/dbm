876a1a7c57b28cf64d99ec98643dfe45841374d7
wayshall <weishao.zeng@gmail.com>
2021-07-08 20:13:10 +0800

    fix: 增加自定义词法解释器，修复druid无法解释特殊字符作为变量的问题
    fix #38

--- src/test/java/org/onetwo/dbm/sharding/ShardingSqlTest.java
+++ src/test/java/org/onetwo/dbm/sharding/ShardingSqlTest.java
@@ -8,6 +8,8 @@ import java.util.Collection;
 import java.util.List;
 
 import org.junit.Test;
+import org.onetwo.common.db.DruidUtils;
+import org.onetwo.common.db.sqlext.ExtQueryUtils;
 import org.onetwo.dbm.utils.DbmUtils;
 import org.springframework.jdbc.core.namedparam.EmptySqlParameterSource;
 
@@ -22,6 +24,10 @@ public class ShardingSqlTest {
 	@Test
 	public void testSelect() {
 		String sql = "select * from user u where u.depart_id = :departId and u.name= :request.userName?likeString and u.age = ?";
+		
+		String countSql = ExtQueryUtils.buildCountSql(sql, "1");
+		System.out.println("countsql: " + countSql);
+		
 		printSql(sql);
 		
 		sql = "select * from user u left join organ org on org.id = u.organ_id where u.depart_id = :departId";
@@ -33,7 +39,8 @@ public class ShardingSqlTest {
 		DbmUtils.parseNamedSql(sql, new EmptySqlParameterSource());
 		
 		String dbType = JdbcConstants.MYSQL;
-		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, dbType);
+//		List<SQLStatement> stmtList = SQLUtils.parseStatements(sql, dbType);
+		List<SQLStatement> stmtList = DruidUtils.parseStatements(sql, dbType);
  
 		ExportTableAliasVisitor visitor = new ExportTableAliasVisitor();
 		MySqlSchemaStatVisitor mySqlStatVisitor = new MySqlSchemaStatVisitor();
