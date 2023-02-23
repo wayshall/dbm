a6f8e309901eb273e1c1f2bf7d50cc8855c9e925
wayshall <weishao.zeng@gmail.com>
2023-02-23 12:57:43 +0800

    fix: add dbm param info2

---
+++ src/main/java/org/onetwo/common/db/dquery/DbmParamInfo.java
@@ -0,0 +1,38 @@
+package org.onetwo.common.db.dquery;
+
+import javax.persistence.EnumType;
+
+import lombok.Data;
+
+/**
+ * 对应 org.onetwo.common.db.dquery.annotation.Param 注解
+ * @author weishao zeng
+ * <br/>
+ */
+@Data
+public class DbmParamInfo {
+
+	private String name;
+	
+	/***
+	 * 如果参数是数组和列表，使用name+index重新生成参数名称
+	 * cardNo in ( ${_func.inParams('cardNo', cardNos.size())} )
+	 * 
+	 * @deprecated 已直接支持List参数
+	 * @return
+	 */
+	@Deprecated
+	private boolean renamedUseIndex = false;
+	
+	private boolean isLikeQuery = false;
+	
+	private EnumType enumType = EnumType.STRING;
+	
+	public EnumType enumType() {
+		return enumType;
+	}
+	
+	public boolean renamedUseIndex() {
+		return renamedUseIndex;
+	}
+}
