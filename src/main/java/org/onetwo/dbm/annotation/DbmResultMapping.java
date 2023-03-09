c44840bf0b1ec8d83da08062b6e29fe858ab0398
wayshall <weishao.zeng@gmail.com>
2023-03-09 10:56:20 +0800

    fix: add comment

--- src/main/java/org/onetwo/dbm/annotation/DbmResultMapping.java
+++ src/main/java/org/onetwo/dbm/annotation/DbmResultMapping.java
@@ -7,6 +7,11 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface DbmResultMapping {
+	/***
+	 * 根据id属性作为区分一条记录的标志，否则使用对象的hashcode
+	 * @author weishao zeng
+	 * @return
+	 */
 	String idField() default "";
 	String columnPrefix() default "";
 	DbmNestedResult[] value();
