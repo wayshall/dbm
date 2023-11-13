e05ba59ba002cee2e26e9b2c5b4f7700cb19a791
wayshall <weishao.zeng@gmail.com>
2023-11-13 18:31:48 +0800

    fix: add sql field check function

--- src/main/java/org/onetwo/common/db/SqlUtils.java
+++ src/main/java/org/onetwo/common/db/SqlUtils.java
@@ -1,5 +1,7 @@
 package org.onetwo.common.db;
 
+import java.util.regex.Pattern;
+
 import org.onetwo.common.utils.StringUtils;
 import org.onetwo.dbm.exception.DbmException;
 import org.springframework.util.ClassUtils;
@@ -8,6 +10,19 @@ final public class SqlUtils {
 	private static final String DRUID_PARSER_CLASS = "com.alibaba.druid.sql.parser.SQLParser";
 	
 	public static final String[] SQL_KEY_WORKDS = new String[]{" ", ";", ",", "(", ")", "'", "\"", "\"\"", "/", "+", "-", "#"};
+	
+	// 定义允许的字符模式（字母、数字、下划线）
+	static final Pattern ALLOWED_FIELD_NAME_PATTERN = Pattern.compile("^[a-zA-Z0-9_]*$");
+
+	public static void checkSQLSafeField(String fieldName) {
+		if(!isSQLSafeField(fieldName)) {
+			throw new DbmException("sql field is unsafe : " + fieldName);
+		}
+    }
+	
+	public static boolean isSQLSafeField(String fieldName) {
+        return ALLOWED_FIELD_NAME_PATTERN.matcher(fieldName).matches();
+    }
 
 	public static String check(String sqlValue){
 		if(StringUtils.isBlank(sqlValue)){
